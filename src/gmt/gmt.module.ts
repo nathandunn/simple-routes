69545ef4cc34f6ba2475d12798e7dcfe70272cfa
Nathan Dunn <ndunn@me.com>
2020-12-05 06:33:48 -0800

    updated the gmt module more

--- src/gmt/gmt.module.ts
+++ src/gmt/gmt.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GmtController } from './gmt/gmt.controller';
+import { GmtService } from './gmt/gmt.service';
 
 @Module({
-  controllers: [GmtController]
+  controllers: [GmtController],
+  providers: [GmtService]
 })
 export class GmtModule {}
